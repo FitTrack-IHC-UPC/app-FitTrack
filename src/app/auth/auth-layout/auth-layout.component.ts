d15f60e65cdd4ae079ce189313f6f19165989b58
SmallGoodSergio <sergioandressc2018@gmail.com>
2024-11-11 22:51:01 -0500

    MENU

--- src/app/auth/auth-layout/auth-layout.component.ts
+++ src/app/auth/auth-layout/auth-layout.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
