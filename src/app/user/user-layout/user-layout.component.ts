f1a8b5c92843d52c91e476ed364350f28bdf58c5
NathaNN73 <105460841+NathaNN73@users.noreply.github.com>
2024-11-11 21:50:04 -0600

    fix: Conflicto arreglado es user-layout

--- src/app/user/user-layout/user-layout.component.ts
+++ src/app/user/user-layout/user-layout.component.ts
@@ -1,17 +1,9 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet } from '@angular/router';
-@Component({
-  selector: 'app-user-layout',
-  standalone: true,
-  imports: [RouterOutlet],
-=======
 import { RouterOutlet, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-user-layout',
   standalone: true,
   imports: [RouterOutlet,RouterLink],
->>>>>>> develop
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.css'
 })
